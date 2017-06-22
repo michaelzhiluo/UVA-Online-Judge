64cf38e6b9e253c322a191cc02db9759e210f738
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-06-22 17:04:46 +0800

    Done with Easy Problems

--- Competitive Programming 3/Chapter 1/Easy/UVA11559.cpp
+++ Competitive Programming 3/Chapter 1/Easy/UVA11559.cpp
@@ -1,16 +1,33 @@
-#include <iosteam>
+#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
 	int numpeople;
-	long int budget;
+	int budget;
 	int numhotels;
 	int numweeks;
-	while(true){
-		scanf("%d %ld %d %d", &numpeople, &budget, &numhotels, &numweeks);
-		
+	int mincost;
+	while(scanf("%d %d %d %d", &numpeople, &budget, &numhotels, &numweeks)!=EOF){
+		mincost = budget + 1;
+		for(int i=0; i<numhotels; i++){
+			int hotelcost;
+			scanf("%d", &hotelcost);
+			for(int j=0; j<numweeks; j++){
+				int temp;
+				scanf("%d", &temp);
+				if(temp >= numpeople && hotelcost*numpeople < mincost){
+					//cout << hotelcost << " " << numpeople << endl;
+					mincost = hotelcost*numpeople;
+				}
+			}
+		}
+		if(mincost > budget){
+			printf("stay home\n");
+		}else{
+			printf("%d\n", mincost);
+		}		
 
 	}
 	
