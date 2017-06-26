3776269a978469bd81c6fcad12674f3e9981e090
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-06-26 16:48:03 +0800

    Updated

--- Competitive Programming 3/Chapter 1/Super Easy/UVA12279.cpp
+++ Competitive Programming 3/Chapter 1/Super Easy/UVA12279.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main(){
-	
-	return 0;
+	int length;
+	int placeholder;
+	int counter = 1;
+	while(true){
+		scanf("%d", &length);
+		if(length ==0){
+			break;
+		}
+		int numzeroes =0;
+		for(int i=0; i<length; i++){
+			scanf("%d", &placeholder);
+			if(placeholder ==0){
+				numzeroes++;
+			}
+		}
+
+		printf("Case %d: %d\n", counter++, length - 2*numzeroes);
+	}
 }
