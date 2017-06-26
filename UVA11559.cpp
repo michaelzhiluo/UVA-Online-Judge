3f2fcc0aa24c8142a1a34806816108b461fc7c7c
Michael Luo <michael.luo123456789@gmail.com>
2017-06-26 08:23:55 +0800

    Update UVA11559.cpp

--- UVA11559.cpp
+++ UVA11559.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 int main(){
-	int numpeople;
-	int budget;
-	int numhotels;
-	int numweeks;
-	int mincost;
+	int numpeople, budget, numhotels, numweeks, mincost;
 	while(scanf("%d %d %d %d", &numpeople, &budget, &numhotels, &numweeks)!=EOF){
 		mincost = budget + 1;
 		for(int i=0; i<numhotels; i++){
