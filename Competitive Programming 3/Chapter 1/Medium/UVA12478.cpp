83b984d63ada9fbe2195ddc063b56ca36c6f76a8
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-06-26 10:22:28 +0800

    Added two more

---
+++ Competitive Programming 3/Chapter 1/Medium/UVA12478.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// Very difficult problem, took me hours to solve. 
+// Please don't look at my solution before staring at the problem for couple hours.
+// This problem will definitely help you make some mental breakthroughs.
+int main(){
+	printf("KABIR\n");
+	return 0;
+}
