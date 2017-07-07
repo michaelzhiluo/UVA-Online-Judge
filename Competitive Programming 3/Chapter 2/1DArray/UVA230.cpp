575279b7289698ec3b53e197e81a55233b7a4294
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-07-07 16:43:18 +0800

    Ripperino

--- Competitive Programming 3/Chapter 2/1DArray/UVA230.cpp
+++ Competitive Programming 3/Chapter 2/1DArray/UVA230.cpp
@@ -4,15 +4,65 @@
 using namespace std;
 
 struct Book{
-    string title, author;
-}
+    char title[1000], author[1000]; 
+    bool operator < (Book& a)
+    {
+        if(strcmp(title, a.title) !=0){
+            return strcmp(title, a.title) <0;
+        }
+        return strcmp(author, a.author) < 0;
+    }
+};
+
+struct charcmp{
+    bool operator()(char *a, char* b){
+        return strcmp(a,b)<0;
+    }
+};
 
 int main(){
-    string input = "\"The Canterbury Tales\" by Chaucer, G.";
-    char a[200], t[200];
-    sscanf(input.c_str(), "\"%[^\"]\" by %[^\n\r]", t, a);
-    printf("%s\n", a);
-    printf("%s\n", t);    
+    char input[2000], a[1000], t[1000];
+    vector<Book> books;
+    map<char*, bool, charcmp> exists;
+    while(true){
+        fgets (input, 2000, stdin);
+        if(strcmp(input, "END\n")==0){
+            break;
+        }
+        string input1 = string(input);
+        sscanf(input1.c_str(), "\"%[^\"]\" by %[^\n\r]", t, a);
+        Book temp;
+        strcpy(temp.title, t);
+        strcpy(temp.author, a);
+        books.push_back(temp);
+    }
+    sort(books.begin(), books.end());
+
+    for(vector<Book>::iterator i = books.begin(); i!=books.end(); i++){
+        exists.insert(pair<char*, int>(i->title, true));
+    }
+
+    while(true){
+        fgets(input, 2000, stdin);
+        if(strcmp(input, "END\n") ==0){
+            break;
+        }
+        string input1 = string(input);
+        sscanf(input1.c_str(), "%s \"%[^\"]\"", t, a);
+        if(!strcmp(t, "BORROW")){
+            exists[a] = false;
+        }else if(!strcmp(t, "RETURN")){
+            exists[a] = true;
+        }else if(!strcmp(t, "SHELVE")){
+
+        }
+
+    }
+    for(map<char*,bool>::iterator it=exists.begin(); it!=exists.end(); it++){
+        cout << it->first << " => " << it->second << endl;
+    }
+    printf("END\n");
+
     
     return 0;
 }
