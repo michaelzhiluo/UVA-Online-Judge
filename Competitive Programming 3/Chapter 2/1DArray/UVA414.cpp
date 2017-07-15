fd0411fbbfaefeaf0ad6ed22e4012b1f9fb16802
Michael Luo <michael.luo@berkeley.edu>
2017-07-15 04:52:15 -0700

    Stupid input tricky whitespace

--- Competitive Programming 3/Chapter 2/1DArray/UVA414.cpp
+++ Competitive Programming 3/Chapter 2/1DArray/UVA414.cpp
@@ -2,11 +2,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 int main(){
-    int numlines;
+    int numlines, left, right;
+    char placeholder[100];
     while(true){
         scanf("%d", &numlines);
+        getchar();
+        if(!numlines){break;}
+        int min = 100;
+        int sum =0;
+        for(int i=0; i<numlines; i++){
+            gets(placeholder);
+            int x =0;
+            while(placeholder[x] == 'X' && placeholder[x]!= '\0'){
+                x++;
+            }
+            left = x;
+            while(placeholder[x] !='X' && placeholder[x] !='\0'){
+                x++;
+            }
+            right = x;
+            min = (right - left) < min ? (right-left) : min;
+            sum += right - left;
+        }
+        printf("%d\n", sum - numlines*min);
     }
     return 0;
 }
