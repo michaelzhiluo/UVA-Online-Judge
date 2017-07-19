86ec4d9064648b7879dc5ce50015779c1c4e0616
Michael Luo - 羅志宇 <michael.luo@berkeley.edu>
2017-07-19 15:51:48 +0800

    Wow presentation error took more time than solving this question

--- Competitive Programming 3/Chapter 2/1DArray/UVA665.cpp
+++ Competitive Programming 3/Chapter 2/1DArray/UVA665.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <bits/stdc++.h>
 
+// Heads up, troll question, you cannot determine UNIQUE counterfeit coint with >, < so only focus on =
 using namespace std;
 int main(){
-	int TC;
+	int TC, n, k, p;
+	char comparator;
 	scanf("%d", &TC);
 	while(TC--){
-		
+		scanf("%d %d", &n, &k);
+		bool notcounterfeit[n+1];
+		for(int i=0; i<n+1; i++){
+			notcounterfeit[i] = false;
+		}
+		for(int x=0; x<k; x++){
+			scanf("%d", &p);
+			int left[p], right[p];
+			for(int i=0; i<p; i++){
+				scanf("%d", &left[i]);
+			}
+			for(int i=0; i<p; i++){
+				scanf("%d", &right[i]);
+			}
+			scanf(" %c", &comparator);
+			if(comparator == '='){
+				for(int i=0; i<p; i++){
+					notcounterfeit[left[i]] = true;
+					notcounterfeit[right[i]] = true;
+				}
+			}
+		}
+		int counter =0;
+		for(int i=1; i<=n; i++){
+			if(!notcounterfeit[i]){ counter++; p =i;}
+		}
+		printf(counter!=1 ? "0\n" : "%d\n", p);
+		if(TC){ printf("\n");}
 	}
 }
